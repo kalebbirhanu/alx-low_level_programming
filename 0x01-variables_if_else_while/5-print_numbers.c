de395e4db7b3b3880ac4607ebdee7faa8b97ce38
kalebbirhanu <birhanukaleb62@gmail.com>
2022-03-14 20:34:31 +0300

    hello world

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+/* more headers goes there */
+int main(void)
+{
+	int x;
+
+	for (x = 0;  x <= 9; x++)
+	{
+	printf("%d", x);
+	}
+	printf('\n');
+	return (0);
+}
