d4b8c60bcf1d78759aab82fa3cd9ac2dd2be4d70
kalebbirhanu <birhanukaleb62@gmail.com>
2022-03-14 20:15:30 +0300

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (succes)
+  */
+int main(void)
+{
+clrscr();
+printf(“TypettttSize (bytes)”);
+printf(“nCharacterttt	%d ”, sizeof(char));
+printf(“nIntegertttt	%d ”, sizeof(int));
+printf(“nLong intttt	%d ”, sizeof(long int));
+printf(“nFloattttt	%d ”, sizeof(float));
+printf(“nDoubletttt	%d ”, sizeof(double));
+printf(“nLong doublettt	%d ”, sizeof(long double));
+	return (0);
+}
